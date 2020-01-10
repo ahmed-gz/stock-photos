3e2bbda3c4b1a7a1e320a47aee8d0c53e04d73be
Ahmed Gazimi <ahmed.gazimi@gmail.com>
2020-01-10 16:49:43 +0100

    [refactor] - Code cleanup and formatting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md" className="app">
+      <Box mx="10px" my="20px">
+        <Typography color="textPrimary">Popular Categories</Typography>
+      </Box>
+    </Container>
   );
-}
+};
 
 export default App;
